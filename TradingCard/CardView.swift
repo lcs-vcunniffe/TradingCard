4934b1a9cea20068adbf9a1b60f01e92d8a6e5a8
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-22 22:01:34 -0500

    Along with a few other things, finished the TitleView shadow.

--- TradingCard/CardView.swift
+++ TradingCard/CardView.swift
@@ -7,26 +7,41 @@
 
 import SwiftUI
 
-struct BackgroundView: View {
+struct CardView: View {
     
     //MARK: Stored properties
     let backgroundColor: Color
+    let backgroundImage: Image
+    let fillColor: Color
     
     //MARK: Computed properties
     var body: some View {
-        ZStack (alignment: .top) {
-            Color.black
-                .ignoresSafeArea()
-            RoundedRectangle(cornerRadius: 10.0)
-                .fill(Color(backgroundColor))
-                .frame(height: 600)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
             VStack {
-                
+                Image("fireBackground")
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                .frame(width: 360, height: 650)
+                Rectangle()
+                    .fill(Color.clear)
+                    .frame(height: 70)
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .strokeBorder(Color.black, lineWidth: 20)
+            VStack {
+               //note to self: this is where the titles/images go
+                ZStack {
+                    Rectangle()
+                        .border(Color.red, width: 5)
+                        .frame(width: 300, height: 200)
+                        .foregroundStyle(Color(fillColor))
+                }
             }
         }
     }
 }
 
 #Preview {
-    BackgroundView(backgroundColor: Color.red)
+    CardView(backgroundColor: Color.red, backgroundImage: Image("fireBackground"), fillColor: Color.textRed)
 }
--- TradingCard/TitlesView.swift
+++ TradingCard/TitlesView.swift
@@ -12,30 +12,56 @@ struct TitlesView: View {
     //MARK: Stored properties
     let fillColor: Color
     let title: String
+    let fontSize: CGFloat
     
     //MARK: Computed properties
     var body: some View {
         ZStack {
-            Capsule()
-                .stroke(Color.black, lineWidth: 2)
-                .frame(width: 375, height: 50)
-            Capsule()
-                .frame(width: 375, height: 50)
+            Capsule(style: RoundedCornerStyle.continuous)
+                .stroke(Color.red, lineWidth: 10)
+                .frame(width: 375, height: 35)
+            Capsule(style: RoundedCornerStyle.continuous)
+                .frame(width: 375, height: 35)
                 .foregroundStyle(Color(fillColor))
+                .overlay(
+                    Capsule(style: RoundedCornerStyle.continuous)
+                        .stroke(Color.black, lineWidth: 3)
+                        .shadow(color: Color.black, radius: 1.5, x: 1.5, y: -1.5)
+                )
+                .overlay(
+                    Capsule(style: RoundedCornerStyle.continuous)
+                        .stroke()
+                        .shadow(color: Color.gray, radius: 1.5, x: 1.5, y: -1.5)
+                )
+                .overlay(
+                    Capsule(style: RoundedCornerStyle.continuous)
+                        .stroke()
+                        .shadow(color: Color.white, radius: 1.5, x: -1.5, y: 1.5)
+                )
+                .overlay(
+                    Capsule(style: RoundedCornerStyle.continuous)
+                        .stroke()
+                        .shadow(color: Color.white, radius: 1.5, x: -1.5, y: 1.5)
+                )
+                .clipShape(Capsule(style: RoundedCornerStyle.continuous))
             HStack {
+                Rectangle()
+                    .fill(Color.clear)
+                    .frame(width: 10)
                 Text("\(title)")
                     .bold()
                     .font(
                         .custom(
                             "Bodoni 72 Oldstyle Bold",
-                            size: 40
+                            size: fontSize
                         )
                 )
+                Spacer()
             }
         }
     }
 }
 
 #Preview {
-    TitlesView(fillColor: Color.titleRed, title: "Shivan Dragon")
+    TitlesView(fillColor: Color.titleRed, title: "Unholy Heat", fontSize: 30)
 }
