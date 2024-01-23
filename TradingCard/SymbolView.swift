4f85b9333d7445bcc5b4cea50e12f737877d925c
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-23 11:15:47 -0500

    Made the SymbolView as a way to put the icons in my structures.

---
+++ TradingCard/SymbolView.swift
@@ -0,0 +1,23 @@
+//
+//  SymbolView.swift
+//  TradingCard
+//
+//  Created by Veda Niav Cunniffe on 2024-01-23.
+//
+
+import SwiftUI
+
+struct SymbolView: View {
+    var body: some View {
+        //MARK: Stored properties
+        
+        //MARK: Computed properties
+        ZStack {
+            
+        }
+    }
+}
+
+#Preview {
+    SymbolView()
+}
