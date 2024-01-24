b818b3c657b7ddad23b33763dbd78138bdc4f08e
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-24 16:01:16 -0500

    Committing a bunch of different changes, the most significant being the start of the list.

---
+++ TradingCard/Models/Symbols.swift
@@ -0,0 +1,24 @@
+//
+//  Symbols.swift
+//  TradingCard
+//
+//  Created by Veda Niav Cunniffe on 2024-01-23.
+//
+
+import SwiftUI
+
+struct Symbols {
+    let circleFill: Color
+    var circleSize: CGFloat
+    let icon: Image
+    var iconWidth: CGFloat
+    let maskShape: any Shape
+}
+
+let fireMana = Symbols(
+    circleFill: Color.manaRed,
+    circleSize: CGFloat,
+    icon: Image,
+    iconWidth: CGFloat,
+    maskShape: any Shape
+)
